cf2d8df3c7b89e0f2b27499eeeffba570b03a2d6
bikeshresthaa <eshbikesh@gmail.com>
2025-08-11 20:46:06 +0545

    remove duplication

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,54 +1,23 @@
 def caesar_cipher(string, shift_pos)
+  shift_pos %= 26
+  encrypted = string.chars.map do |char|
+        if char.ord.between?("A".ord, "Z".ord)
+          base_ord = "A".ord
+          (((char.ord + shift_pos - base_ord)%26) + base_ord).chr
+        elsif char.ord.between?("a".ord, "z".ord)
+          base_ord = "a".ord 
+          (((char.ord + shift_pos - base_ord)%26) + base_ord).chr
+        else
+          char
+        end
+  end 
+
+  puts "The encrypted string is : #{encrypted.join}"
   
-  input = string.split('')
-  if shift_pos > 0
-    output = right_shift(input, shift_pos)
-  else
-    output = left_shift(input, shift_pos.abs)
-  end
-
-  puts "The encrypted string is : #{output.join}"
-  
 end
 
 
-def left_shift(array, shift_pos)
-  output = []
-  array.each do |character|
-    if character.ord.between?("A".ord, "Z".ord)
-      shifted = character.ord - shift_pos
-      shifted += 26 if shifted < "A".ord
-      output << shifted.chr
-    elsif character.ord.between?("a".ord, "z".ord)
-      shifted = character.ord - shift_pos
-      shifted += 26 if shifted < "a".ord
-      output << shifted.chr
-    else
-      output << character
-    end
-    
-  end
-  return output
-end
 
-def right_shift(array, shift_pos)
-  output = []
-  array.each do |character|
-    if character.ord.between?("A".ord, "Z".ord)
-      shifted = character.ord + shift_pos
-      shifted -= 26 if shifted > "Z".ord
-      output << shifted.chr
-    elsif character.ord.between?("a".ord, "z".ord)
-      shifted = character.ord + shift_pos
-      shifted -= 26 if shifted > "z".ord
-      output << shifted.chr
-    else
-      output << character
-    end
-    
-  end
-  return output
-end
 
 
 caesar_cipher("Hello World!", 3)
